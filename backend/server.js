4b6ff9b55fc52046c1c4896f745cdc455878517b
Sensei WU <stefanmatei62@yahoo.ro>
2026-03-21 22:25:52 +0200

    token

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "token"]
 };
 
 // Middlewares
